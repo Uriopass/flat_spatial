e12448061577b19645fdfaf5bc144117c6976532
Douady Pâris <paris.douady@hotmail.fr>
2020-05-26 22:21:38 +0200

    create basic example

--- README.md
+++ README.md
@@ -61,7 +61,7 @@ fn main() {
     
     let mut g: DenseGrid<()> = DenseGrid::new(10);
     let a = g.insert([3.0, 3.0], ());
-    let b = g.insert([12.0, -8.0], ());
+    let _b = g.insert([12.0, -8.0], ());
     
     let around: Vec<_> = g.query_around([2.0, 2.0], 5.0)
                           .map(|(id, _pos)| id)
---
+++ examples/basic_example.rs
@@ -0,0 +1,14 @@
+fn main() {
+    use flat_spatial::DenseGrid;
+
+    let mut g: DenseGrid<()> = DenseGrid::new(10);
+    let a = g.insert([3.0, 3.0], ());
+    let _b = g.insert([12.0, -8.0], ());
+
+    let around: Vec<_> = g
+        .query_around([2.0, 2.0], 5.0)
+        .map(|(id, _pos)| id)
+        .collect();
+
+    println!("{:?} = {:?}", vec![a], around);
+}
