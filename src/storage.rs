6c39503f54d60fea8ae0f04967a3acc06fd36fa7
Paris DOUADY <paris.douady@hotmail.fr>
2023-05-01 01:03:46 +0200

    expose storage funs

--- src/storage.rs
+++ src/storage.rs
@@ -27,12 +27,12 @@ pub(crate) fn cell_range((min_x, min_y): CellIdx, (max_x, max_y): CellIdx) -> XY
 #[derive(Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct SparseStorage<T: Default> {
-    cell_size: i32,
-    cells: fnv::FnvHashMap<CellIdx, T>,
+    pub cell_size: i32,
+    pub cells: fnv::FnvHashMap<CellIdx, T>,
 }
 
 impl<T: Default> SparseStorage<T> {
-    pub(crate) fn new(cell_size: i32) -> Self {
+    pub fn new(cell_size: i32) -> Self {
         assert!(
             cell_size > 0,
             "Cell size ({}) cannot be less than or equal to zero",
@@ -44,28 +44,28 @@ impl<T: Default> SparseStorage<T> {
         }
     }
 
-    pub(crate) fn cell_size(&self) -> i32 {
+    pub fn cell_size(&self) -> i32 {
         self.cell_size
     }
 
-    pub(crate) fn modify(&mut self, mut f: impl FnMut(&mut T) -> bool) {
+    pub fn modify(&mut self, mut f: impl FnMut(&mut T) -> bool) {
         self.cells.retain(move |_, cell| !f(cell));
     }
 
-    pub(crate) fn cell_mut<V2: Vec2>(&mut self, pos: V2) -> (CellIdx, &mut T) {
-        let id = self.cell_id(pos);
+    pub fn cell_mut<V2: Vec2>(&mut self, pos: V2) -> (CellIdx, &mut T) {
+         let id = self.cell_id(pos);
         (id, self.cells.entry(id).or_default())
     }
 
-    pub(crate) fn cell_mut_unchecked(&mut self, id: CellIdx) -> &mut T {
+    pub fn cell_mut_unchecked(&mut self, id: CellIdx) -> &mut T {
         self.cells.entry(id).or_default()
     }
 
-    pub(crate) fn cell(&self, id: CellIdx) -> Option<&T> {
+    pub fn cell(&self, id: CellIdx) -> Option<&T> {
         self.cells.get(&id)
     }
 
-    pub(crate) fn cell_id<V2: Vec2>(&self, pos: V2) -> CellIdx {
+    pub fn cell_id<V2: Vec2>(&self, pos: V2) -> CellIdx {
         (
             pos.x() as i32 / self.cell_size - if pos.x() < 0.0 { 1 } else { 0 },
             pos.y() as i32 / self.cell_size - if pos.y() < 0.0 { 1 } else { 0 },
